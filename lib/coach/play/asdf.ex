6ebfa883042bdc2e9e167454718d0fb12d8c84b4
Cody Poll <cjpoll@gmail.com>
2017-07-26 16:53:10 -0600

    Add ability to specify asdf binary

--- lib/coach/play/asdf.ex
+++ lib/coach/play/asdf.ex
@@ -1,33 +1,33 @@
 defmodule Coach.Play.ASDF do
   alias Coach.Cmd.Shell
 
-  def add_plugin(plugin, repo) do
+  def add_plugin(plugin, repo, bin \\ "asdf") do
     Shell.new()
-    |> Shell.with_command("asdf")
+    |> Shell.with_command(Coach.Path.path(bin))
     |> Shell.with_value("plugin-add")
     |> Shell.with_value(plugin)
     |> Shell.with_value(repo)
   end
 
-  def install_version(plugin, version) do
+  def install_version(plugin, version, bin \\ "asdf") do
     Shell.new()
-    |> Shell.with_command("asdf")
+    |> Shell.with_command(Coach.Path.path(bin))
     |> Shell.with_value("install")
     |> Shell.with_value(plugin)
     |> Shell.with_value(version)
   end
 
-  def set_global(plugin, version) do
+  def set_global(plugin, version, bin \\ "asdf") do
     Shell.new()
-    |> Shell.with_command("asdf")
+    |> Shell.with_command(Coach.Path.path(bin))
     |> Shell.with_value("global")
     |> Shell.with_value(plugin)
     |> Shell.with_value(version)
   end
 
-  def set_local(plugin, version) do
+  def set_local(plugin, version, bin \\ "asdf") do
     Shell.new()
-    |> Shell.with_command("asdf")
+    |> Shell.with_command(Coach.Path.path(bin))
     |> Shell.with_value("local")
     |> Shell.with_value(plugin)
     |> Shell.with_value(version)
